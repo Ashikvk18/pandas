2b468d5076d7de0a6fd906741ed1f8669ad249f7
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-26 20:20:51 -0500

    time metjod

--- methods.py
+++ methods.py
@@ -1,7 +1,13 @@
 import pandas as pd
 data1 = {
-    "Time": [1, 2, 3, 4, 5],
-    "Value": [10, None, 30, None, 50]
+   "Time": pd.to_datetime([
+        "2024-01-01",
+        "2024-01-02",
+        "2024-01-03",
+        "2024-01-04",
+        "2024-01-05"
+    ]),
+    "Value": [910, None, 307, None, 502]
 }
 print(data1)
 df = pd.DataFrame(data1)
@@ -9,6 +15,6 @@ print(df)
 
 print ("after interpolation")
 
-df["Value"] = df["Value"].interpolate(method='linear')
+df["Value"] = df["Value"].interpolate(method="time")
 print(df)
 
