143b8420d5f041024cd631770b62f99cd41d480c
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-26 21:19:05 -0500

    update

--- operations.py
+++ operations.py
@@ -4,7 +4,7 @@ data1 = {
     "Age" : [10, None, 20,30,40,50,60,70,80,90,100,110],
     "City" : ["Texas", None, "California", "London", "Paris", "Tokyo", "Berlin", "Madrid", "New York", "London", "Paris", "Tokyo"],
     "Salary" : [1000, None, 200000,3000,4000,5000,6000,7000,8000,9000,10000,11000],
-    "Performance Score" : [88, None, 99, 91, 49, 55, 66, 77, 88, 99, 10, 11]
+    "Performance Score" : [88, None, 99, 91, 49, 45, 62, 97, 78, 83, 92, 51]
 }
 print(data1)
 df = pd.DataFrame(data1)
